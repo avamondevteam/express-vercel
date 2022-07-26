ce932b3ccb539eb5abe675cbd861a108687ad677
Khanhdev191 <70697405+Khanhdev191@users.noreply.github.com>
2022-07-26 18:25:10 +0700

    update

--- api/product.js
+++ api/product.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient
 
-
-
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
+    
     var url = 'mongodb+srv://khanhdev191:zxcv123321@cluster0.mznq0.mongodb.net/Khanhdev191?retryWrites=true&w=majority'
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
@@ -13,10 +12,13 @@ router.get("/", async (req, res) => {
       dbo.collection("Products").find({}).toArray(function(err, result) {
         if (err) throw err;
         db.close();
+        // console.log((req.body.project));
+        // res.send(JSON.stringify(req.body));
         res.json({
           status: 200,
           message: "Get data has successfully",
-          data: {result}
+          data: {result},
+          // jsonData: {req.body}
         });
       });
     });
